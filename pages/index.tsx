d103e768e8e464d68be545aa377e0e8305c79590
Ohkubo KOHEI <kuboon@trick-with.net>
2022-03-07 05:52:36 +0000

    fix design

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,6 +47,20 @@ const startAt = Temporal.Now.plainTimeISO();
 const time2 = startAt.add({ minutes: 5 });
 const defaultText = `${hourMin(startAt)} 開始
 0:01 １分後
+0:01 １分後
+0:01 １分後
+0:01 １分後
+0:01 １分後
+0:01 １分後
+0:01 １分後
+0:01 １分後
+0:01 １分後
+0:01 １分後
+0:01 １分後
+0:01 １分後
+0:01 １分後
+0:01 １分後
+0:01 １分後
 0:02 その2分後
 
 ${hourMin(time2)} 空行のあとは時刻
@@ -66,28 +80,18 @@ export default function Home() {
     <>
       <Head>
         <title>Event Keeper</title>
-        <meta name="viewport" content="user-scalable=no"></meta>
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,user-scalable=no"
+        >
+        </meta>
         <link href="/style.css" rel="stylesheet" />
       </Head>
       <div id="slideout" data-slideout-panel>
         <div data-slideout-header>
           <h3>setting</h3>
           <a href="#" data-slideout-close>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="feather feather-x"
-            >
-              <line x1="18" y1="6" x2="6" y2="18"></line>
-              <line x1="6" y1="6" x2="18" y2="18"></line>
-            </svg>
+            x
           </a>
         </div>
         <textarea
@@ -99,21 +103,37 @@ export default function Home() {
         </textarea>
       </div>
 
-      <a class="button" href="#slideout" data-slideout-toggle>&gt;</a>
-      {showClock && <ClockPart timers={timers}/>}
+      {showClock && <ClockPart timers={timers} />}
     </>
   );
 }
 function ClockPart({ timers }: { timers: Timer[] }) {
   const now = useTimer();
-  return      <div class="boxes">
-        <div class="box clock">
-          <Clock now={now} />
-        </div>
-        <div class="box timers">
-          <TimerComponent timers={timers} now={now} />
-        </div>
+  let line = 0;
+  for (let i = 0; i < timers.length; i++) {
+    const { at } = timers[i];
+    const { at: next } = timers[i + 1] || {};
+    if (
+      next &&
+      Temporal.PlainTime.compare(at, now) < 0 &&
+      Temporal.PlainTime.compare(now, next) < 0
+    ) {
+      line = i;
+    }
+  }
+  return (
+    <div class="boxes">
+      <div class="box clock">
+        <a id="slideout-button" href="#slideout" data-slideout-toggle>
+          setting&gt;
+        </a>
+        <Clock now={now} />
       </div>
+      <div class="box timers">
+        <TimerComponent timers={timers} line={line} />
+      </div>
+    </div>
+  );
 }
 function useTimer() {
   const [now, setNow] = useState(Temporal.Now.plainTimeISO());
@@ -121,7 +141,6 @@ function useTimer() {
     const timer = setInterval(() => {
       setNow(Temporal.Now.plainTimeISO());
     }, 1000);
-    console.log('timer', timer);
     return () => {
       clearInterval(timer);
     };
@@ -131,25 +150,41 @@ function useTimer() {
 function Clock({ now }: { now: Temporal.PlainTime }) {
   return (
     <p id="now">
-      <svg version="1.2" viewBox="0 0 40 40" style="width: 100%" xmlns="http://www.w3.org/2000/svg" >
-        <text id="t1" y="20" >{hourMin(now, true)}</text>
+      <svg
+        version="1.2"
+        viewBox="0 0 40 20"
+        style="width: 100%; height: 50%"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <text id="t1" y="10">{hourMin(now, true)}</text>
       </svg>
     </p>
   );
 }
+
+let lastLine = -1;
 function TimerComponent(
-  { timers, now }: { timers: Timer[]; now: Temporal.PlainTime },
+  { timers, line }: { timers: Timer[]; line: number },
 ) {
   const ret: h.JSX.Element[] = [];
-  for (let i = 0; i < timers.length; i++) {
-    const { at, text } = timers[i];
-    const { at: next } = timers[i + 1] || {};
-    const showLine = next &&
-      Temporal.PlainTime.compare(at, now) < 0 &&
-      Temporal.PlainTime.compare(now, next) < 0;
-    ret.push(<p class={showLine ? "line" : ""}>{hourMin(at)} {text}</p>);
+  if (lastLine != line && window.document) {
+    document.getElementById("line")?.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
   }
-  return <>{ret}</>;
+  lastLine = line;
+  return (
+    <>
+      {timers.map(({ at, text }, i) => (
+        <p
+          id={i == line ? "line" : undefined}
+        >
+          {hourMin(at)} {text}
+        </p>
+      ))}
+    </>
+  );
 }
 
 export const config: PageConfig = { runtimeJS: true };
